e7f010ba6b04af4368711996c271317ecdc89de2
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2025-05-22 13:13:16 +0700

    feat: update MLflow tracking URI and model reference in prediction function

--- app.py
+++ app.py
@@ -1,42 +1,27 @@
 from pprint import pprint
 from flask import Flask, jsonify, request
 import pickle
-import numpy as np
 import mlflow
 from mlflow.tracking import MlflowClient
-import pandas as pd
 
 
-# โหลด vectorizer และ selector
 with open("vectorizer.pkl", "rb") as f:
     vectorizer = pickle.load(f)
 
 with open("selector.pkl", "rb") as f:
     selector = pickle.load(f)
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000") 
+mlflow.set_tracking_uri("https://mlflow.paperlesstransform.online") 
 
 client = MlflowClient()
 
-# experiment = client.get_experiment("268375058792484634")  # Replace with your experiment ID
-
-# runs = client.search_runs(
-#         experiment_ids=[experiment.experiment_id],
-#         order_by=["metrics.test_accuracy DESC"],  # Sort by test_accuracy
-#         max_results=1  
-# )
-
-# best_run = runs[0]
-# best_run_id = best_run.info.run_id
-# artifacts_path = client.list_artifacts(best_run_id)
-
-
 def predict_mlflow(text):
 
-    logged_model = 'runs:/3a4e5743e779467d82e656c738fa56cb/XGBoost_Model'
+    logged_model = 'runs:/8c35e495fe024ad988f8bc9494271d4c/XGBoost_Model'
     loaded_model = mlflow.pyfunc.load_model(logged_model)
 
-    if  not isinstance(text, list):
+    # Convert a raw text to a list
+    if not isinstance(text, list):
         text = [text]
 
     X_test_tfidf = vectorizer.transform(text)  
@@ -72,8 +57,6 @@ def predict():
     result = predict_mlflow(body["input"]).tolist()
 
     return {"status" : "success", "result": result}
-
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9000)
